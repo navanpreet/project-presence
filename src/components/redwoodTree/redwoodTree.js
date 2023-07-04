86733ee845119d962fd30a8ee8537e4eb9cbc573
navanpreet <119543066+whatisartanyway@users.noreply.github.com>
2023-07-04 11:08:24 -0700

    init

---
+++ src/components/redwoodTree/redwoodTree.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RedwoodTree = () => {
+    return (
+        <div className="redwood-tree">
+            <div className="trunk"></div>
+            <div className="branch branch1"></div>
+            <div className="branch branch2"></div>
+            <div className="branch branch3"></div>
+            {/* Add more branches as needed */}
+        </div>
+    );
+};
+
+export default RedwoodTree;
